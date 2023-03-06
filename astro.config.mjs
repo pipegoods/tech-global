66b06e545b69690bfa39a5dfa8645688de91ff29
pipegoods <pipe.jaider@gmail.com>
2023-03-05 19:16:54 -0500

    🐛  fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://tech-global.vercel.app/',
   integrations: [sitemap(), tailwind()]
 });
